dd261d80f5c2a185519559b6e14c055c74673085
YoonJY <yoon_jaeyeong#naver.com>
2023-06-19 13:20:27 +0900

    230619

--- js/custom.js
+++ js/custom.js
@@ -6,18 +6,18 @@ $(function () {
             : $('.header').removeClass('on');
     });
 
-    // _se_가 붙은곳은 on을 붙여 애니메이션을 하겠다
+
     $(window).on('scroll', function () {
         let sct = $(window).scrollTop();
         $('._se_').each(function () {
             if (sct + $(window).innerHeight() - 200 > $(this).offset().top) {
-                $(this).addClass('on');
+                $(this).addClass('on')
             } else {
-                $(this).removeClass('on');
+                $(this).removeClass('on')
             }
-        });
-    });
-    //const mainSlide = $('.main_slide').slick({});
+        })
+    })
+
     const mainSlide = new Swiper('.main_slide', {
         loop: true,
         parallax: true,
@@ -27,26 +27,66 @@ $(function () {
             disableOnInteraction: false,
         },
         slideActiveClass: 'on',
+
     });
 
     $('.main_visual .arrows .left').on('click', function () {
         mainSlide.slidePrev();
     });
+
     $('.main_visual .arrows .right').on('click', function () {
         mainSlide.slideNext();
     });
 
-
     const noticeSlide = new Swiper('.notice_slide', {
         loop: true,
-        slidesPerView: "2",
-        spaceBetween: 30,
+        slidesPerView: 1,
+        spaceBetween: 0,
+
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            }
+        }
     });
 
     $('.main_notice .arrows .left').on('click', function () {
         noticeSlide.slidePrev();
     });
+
     $('.main_notice .arrows .right').on('click', function () {
         noticeSlide.slideNext();
     });
-})
+
+    $('.mobile_btn').on('click', function () {
+        $('.gnb').toggleClass('on');
+        $('.header').toggleClass('oo');
+    });
+
+    $('.gnb .main_menu>li>a').on('click', function (e) {
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+            $(this)
+                .next()
+                .stop()
+                .slideToggle();
+            // $(this)
+            //     .parent()
+            //     .siblings()
+            //     .find('.sub_menu')
+            //     .stop()
+            //     .slideUp();
+        }
+    });
+
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on')
+    });
+
+    $('.gnb').on('wheel', function (e) {
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+        }
+    })
+});
